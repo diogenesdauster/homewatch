2827f1b1f2d345a063490a4b8b6eb0206dd1482f
dauster <wall-e@dauster.local>
2020-02-05 22:40:45 -0300

    feat: add Material-ui theme

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,6 @@
 /* global document */
 import React from 'react';
 import { render } from 'react-dom';
-import HelloWord from './helloWorld.jsx';
+import App from './app.jsx';
 
-render(<HelloWord />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
